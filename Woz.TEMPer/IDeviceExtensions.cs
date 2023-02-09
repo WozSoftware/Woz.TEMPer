6c34051033b738d22895cc50252a6fed9904a3f3
Lewis Edwards <lewis@9thcircle.net>
2023-02-09 18:49:41 +1100

    Modifications to support TEMPer Gold V31 (metal body) and X V31 (white body with button and humidity sensor).

--- Woz.TEMPer/IDeviceExtensions.cs
+++ Woz.TEMPer/IDeviceExtensions.cs
@@ -6,12 +6,16 @@ namespace Woz.TEMPer
     {
         public static SensorType DetermineType(this IDevice device)
         {
-            switch (device?.ConnectedDeviceDefinition?.ProductName)
+            if(device.ConnectedDeviceDefinition?.ProductName == "TEMPerV1.4")
+                return SensorType.TEMPerV14;
+            
+            if (device.ConnectedDeviceDefinition is {ProductId: 8455, VendorId: 16701})
             {
-                case "TEMPerV1.4":
-                    return SensorType.TEMPerV14;
+                //Manufacturer and product name are corrupt, no easy way to differentiate GoldV31 and XV31
+                //We can just use the XV31 code and throw away the humidity if it's not available
+                return SensorType.TEMPerXV31;
             }
-
+            
             return SensorType.Unknown;
         }
     }
--- Woz.TEMPer/SensorResult.cs
+++ Woz.TEMPer/SensorResult.cs
@@ -10,6 +10,9 @@ namespace Woz.TEMPer
         public static SensorResult Create(IDevice device, decimal temperature)
             => new SensorResult(device, Result<decimal, string>.Create(temperature));
 
+        public static SensorResult Create(IDevice device, decimal temperature, decimal humidity)
+            => new SensorResult(device, Result<decimal, string>.Create(temperature), Result<decimal, string>.Create(humidity));
+
         public static SensorResult Error(IDevice device, string errorMessage)
             => new SensorResult(device, Result<decimal, string>.Raise(errorMessage));
 
@@ -21,9 +24,19 @@ namespace Woz.TEMPer
             Result = result;
         }
 
+        private SensorResult(IDevice device, Result<decimal, string> result, Result<decimal, string> humidity)
+        {
+            SensorType = device.DetermineType();
+            DeviceId = device.DeviceId;
+            Manufacturer = device.ConnectedDeviceDefinition?.Manufacturer ?? Unknown;
+            Result = result;
+            HumidityResult = humidity;
+        }
+
         public SensorType SensorType { get; }
         public string DeviceId { get; }
         public string Manufacturer { get; }
         public Result<decimal, string> Result { get; }
+        public Result<decimal, string> HumidityResult { get; }
     }
 }
